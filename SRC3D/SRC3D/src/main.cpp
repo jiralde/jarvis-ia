d6b8a659d73514d6e4b11bbad0855d5a66744d8f
Giovanni Peirone <mgpeirone@escuelasproa.edu.ar>
2024-05-03 20:32:36 -0300

    as

--- SRC3D/SRC3D/src/main.cpp
+++ SRC3D/SRC3D/src/main.cpp
@@ -6,6 +6,12 @@
 using namespace std;
 
 
+static int createShader(const string& vertexShader, const string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+}
+
+
 int main(void)
 {
     GLFWwindow* window;
@@ -36,12 +42,34 @@ int main(void)
 
     cout << glGetString(GL_VERSION) << "\n";
 
+    float positions[6] = {
+        -0.5f, -0.5f, 
+         0.0f,  0.5f, 
+         0.5f,  0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glDisableVertexAttribArray(0);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         //RENDERIZADO
-
         glClear(GL_COLOR_BUFFER_BIT);
+
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
