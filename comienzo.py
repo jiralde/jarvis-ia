b032809889b5bd41a0133d8f1e23a79d697b3bc0
Jordan <jiralde@escuelasproa.edu.ar>
2024-05-15 20:17:36 -0300

    Update comienzo.py

--- comienzo.py
+++ comienzo.py
@@ -1,26 +1,40 @@
 #priemera red neuronal(modelo 1)
 import tensorflow as tf
 import numpy as np
-#import matplotlib.pyplot as plt
+import keras
+from keras import layers
+import matplotlib.pyplot as plt
+
+
 celsius = np.array([-40, -10, 0, 8, 15, 22, 38], dtype=float)
 fahrenheit = np.array([-40, 14, 32, 46, 59, 72, 100], dtype=float)
 
-capa = tf.keras.layers.dense(units=1, input_shape=[1])
-modelo =tf.keras.sequential([capa])
+#
+
+capa = keras. layers.Dense(units=1, input_shape=[1])
+modelo = keras.Sequential([capa])
 
 modelo.compile(
-    optimizer=tf.keras.optimizers.Adam(0.1),
-    loss='mean_sqared_error'
+    optimizer= keras.optimizers.Adam(1),
+    loss='mean_squared_error'
 )
-print("Entrenamiento")
+print('''
+Entrenamiento
+''')
 
 historial = modelo.fit(celsius, fahrenheit, epochs=1000, verbose=False)
-print("Modelo entrenado")
+print('''
+Modelo Entrenado
+''')
 
-#plt.xlabel("# Epoca")
-#plt.ylabel("Magnitud de pérdida")
-#plt.plot(historial.history["loss"])
+print('''
+Prediccion
+''')
 
-print("Predición")
-resultado = modelo.predict([100.0])
+resultado = modelo.predict([200.0])
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud Perdida")
+plt.plot(historial.history["loss"])
+print()
+print(capa.get_weights())
 print("El resultado es " + str(resultado) + " fahrenheit")
